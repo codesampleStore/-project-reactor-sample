f96cf1f709cf59421410b7380160dd5c20c878fa
marcinduchinski <marcinduchinski@pl.ibm.com>
2024-05-09 14:54:58 +0000

    init

---
+++ src/main/java/sample/com/projectreactorsample/ProjectReactorSampleApplication.java
@@ -0,0 +1,13 @@
+package sample.com.projectreactorsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReactorSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectReactorSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sample/com/projectreactorsample/ProjectReactorSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.com.projectreactorsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactorSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
